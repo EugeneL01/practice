8aabdcbdce48de43192dd1b379348542436f0166
EugeneL01 <eugene@webwiser.io>
2022-02-11 16:46:28 +0200

    Shape Appear

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 let start = new Date().getTime();
 
+const makeShapeAppear = () => {
+    document.getElementById("shape").style.display = "block";
+
+    start = new Date().getTime();
+}
+
+setTimeout(makeShapeAppear, Math.random() * 2000);
+
 document.getElementById("shape").onclick = () => {
     document.getElementById("shape").style.display = "none";
 
     let end = new Date().getTime();
     let timeTaken = (end - start)/1000;
     document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+    makeShapeAppear();
 }
